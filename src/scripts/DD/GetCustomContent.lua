3170102d1e0ae76da2a192836ae8b62596c3ce32
nerble <philosophyofimage@gmail.com>
2023-02-06 12:29:41 +1300

    Nerble dev 1 (#25)
    * Updates
    
    * A number of updates and improvements
    
    * Fixed image names for mobs with 's  in their names
    
    * Update to 0.0.5
    
    * New mountain sector picture
    
    * Fixed custom content downloader (hopefully)

--- src/scripts/DD/GetCustomContent.lua
+++ src/scripts/DD/GetCustomContent.lua
@@ -1,40 +1,69 @@
-function get_custom_content ()
-        local lfs = require "lfs"
-        -- This script generates a list of all custom content files: https://www.dragons-domain.org/main/gui/custom/files.php
-        local filelist = getMudletHomeDir() .. "/DD_GUI/custom_filelist.txt"
-        local filelist_url = 'https://www.dragons-domain.org/main/gui/custom/files.php'
+function get_custom_content()
+    local lfs = require "lfs"
+    local filelist = getMudletHomeDir() .. "/DD_GUI/custom_filelist.txt"
+    local filelist_url = 'https://www.dragons-domain.org/main/gui/custom/files.php'
+    local lines = {}
 
-        downloadFile(filelist, filelist_url)
-        cecho("\n\n<white>Downloading any new custom content...\n")
-     
-end
-
-function get_filelist_files ()
-        local filelist = getMudletHomeDir() .. "/DD_GUI/custom_filelist.txt"
+    cecho("\n\n<white>Checking for new custom content...\n")
+    downloadFile(filelist, filelist_url)
+    tempTimer(3,
+      function()
         if (file_exists(filelist)) then
-                local lines = {}
-                lines = lines_from(filelist)
-                --cecho("\n<white>Updating custom content...\n")
-                for k,v in pairs(lines) do
-                        local result = split_str(v, '|')
-                        --result[1] now holds the file size, result[2] holds the file name
-                        --display("Remote file size: " .. result[1] .. " for " .. result[2])
-                        local saveto = getMudletHomeDir().."/DD_GUI/" .. result[2]
-                        local filesize_v = lfs.attributes (saveto, "size")
-                        
-                        local url = "https://www.dragons-domain.org/main/gui/custom/" .. result[2]
-                        
-                        if (file_exists(saveto)) and (tonumber(result[1]) ~= tonumber(filesize_v)) then
-                            --cecho("File exists locally BUT IS DIFFERENT SIZE for " .. result[2] .. "\n")
-                                downloadFile(saveto, url)
-                        end
-                        
-                        if (filesize_v == nil) then
-                          --cecho("File does not exist locally for " .. result[2] .. "\n")
-                                downloadFile(saveto, url)
-                        end
-                end      
+          lines = lines_from(filelist)
+        else
+          cecho("\n<white>Custom content unable to be checked.\n")
+          return
         end
-end
 
-registerAnonymousEventHandler("sysDownloadDone", "get_filelist_files")
+        -- A table to store the results of each download
+        local results = {}
+        local dl_count = 0
+
+        for k,v in pairs(lines) do
+          --display("k: " .. k .. " v: " .. v)
+          local result = split_str(v, '|')
+          --result[1] now holds the file size, result[2] holds the file name
+          --display("Remote file size: " .. result[1] .. " for " .. result[2] .. "\n")
+          local saveto = getMudletHomeDir().."/DD_GUI/" .. result[2]
+          local filesize_v = lfs.attributes (saveto, "size")
+          local url = "https://www.dragons-domain.org/main/gui/custom/" .. result[2]
+
+          if (file_exists(saveto)) and (tonumber(result[1]) ~= tonumber(filesize_v)) then
+              --cecho("File exists locally BUT IS DIFFERENT SIZE for " .. result[2] .. "\n")
+              --cecho("Downloading: " .. saveto .. "\n")
+              local success = downloadFile(saveto, url)
+              dl_count = dl_count + 1
+              results[k] = { url = url, path = saveto, success = success }
+          end
+
+          if (filesize_v == nil) then
+              --cecho("File does not exist locally for " .. result[2] .. "\n")
+              --cecho("Downloading: " .. saveto .. "\n")
+              local success = downloadFile(saveto, url)
+              dl_count = dl_count + 1
+              results[k] = { url = url, path = saveto, success = success }
+          end
+        end
+
+        -- Print the results of each download
+        if (dl_count == nil) then
+          dl_count = 0
+        end
+
+        if (tonumber(dl_count) > 0) then
+          cecho("\n<white>Found new custom content to download.\n")
+          for i, result in pairs(results) do
+            if (result.success == true) then
+              local message = string.format("<white>Downloaded:<red> %s\n", result.url)
+              cecho("" .. message)
+            end
+          end
+        else
+          cecho("\n<white>No new custom content.\n")
+        end
+
+      end
+      )
+  end
+
+--registerAnonymousEventHandler("sysDownloadDone", "get_filelist_files")
